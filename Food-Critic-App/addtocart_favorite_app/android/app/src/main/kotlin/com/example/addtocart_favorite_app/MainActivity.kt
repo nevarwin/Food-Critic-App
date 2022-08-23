d6c2e6a0497909640cc5ef4862901b1601d35ec9
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2022-08-23 17:54:56 +0800

    removed wriong folders

--- Food-Critic-App/addtocart_favorite_app/android/app/src/main/kotlin/com/example/addtocart_favorite_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.addtocart_favorite_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Food-Critic-App/flutter_tabbar/android/app/src/main/kotlin/com/example/flutter_tabbar/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_tabbar
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
